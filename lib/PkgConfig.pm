1a388aacfe0c6858cbea682a5e4be532b5c9ac45
Graham Ollis <plicease@cpan.org>
2014-02-08 03:43:17 -0500

    version 0.06320

--- lib/PkgConfig.pm
+++ lib/PkgConfig.pm
@@ -18,7 +18,7 @@ package PkgConfig::UDefs;
 package PkgConfig;
 
 #First two digits are Perl version, second two are pkg-config version
-our $VERSION = '0.06220';
+our $VERSION = '0.06320';
 
 require 5.006;
 
