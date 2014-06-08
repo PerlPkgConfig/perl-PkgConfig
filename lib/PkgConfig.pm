0f78ba5b02cce8f027c41f128ebc0915964ec8b3
Graham Ollis <perl@wdlabs.com>
2014-06-08 08:31:22 -0400

    versioin 0.07620_01

--- lib/PkgConfig.pm
+++ lib/PkgConfig.pm
@@ -18,7 +18,7 @@ package PkgConfig::UDefs;
 package PkgConfig;
 
 #First two digits are Perl version, second two are pkg-config version
-our $VERSION = '0.07620';
+our $VERSION = '0.07620_01';
 
 require 5.006;
 
