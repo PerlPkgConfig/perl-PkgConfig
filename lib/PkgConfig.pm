093b7a0f2368a5a02aa82ff240d77d2cba25094e
Graham Ollis <perl@wdlabs.com>
2015-02-23 13:58:44 -0500

    version 0.08826

--- lib/PkgConfig.pm
+++ lib/PkgConfig.pm
@@ -20,7 +20,7 @@ package
 package PkgConfig;
 
 #First two digits are Perl version, second two are pkg-config version
-our $VERSION = '0.08726';
+our $VERSION = '0.08826';
 
 use strict;
 use warnings;
